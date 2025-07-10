9f65e4f92665e1957a3f9345517a308ea2d2809d
zosia <zofiakindeusz@gmail.com>
2025-07-10 20:55:21 +0200

    defined available moves

--- popuate.sql
+++ popuate.sql
@@ -5,9 +5,21 @@ INSERT INTO MOVES (col, row, repeatable, sym)
     VALUES  (0, 1, FALSE, FALSE),
             (0, 1, FALSE, TRUE),
             (1, 1, FALSE, TRUE),
-            (0, 1, TRUE, TRUE),
+            (0, 1, TRUE, TRUE), 
             (1, 1, TRUE, TRUE),
             (1, 2, FALSE, TRUE),
             (2, 1, FALSE, TRUE);
 
-INSERT INTO PLAYERS (name) VALUES ('Z');
+INSERT INTO AVAILABLEMOVES (ptid, mid)
+    VALUES (1, 1),
+           (2, 4),
+           (3, 4),
+           (3, 5),
+           (4, 2),
+           (4, 3),
+           (5, 7),
+           (5, 8),
+           (6, 3);
+
+INSERT INTO PLAYERS (name) VALUES ('Zosia');
+INSERT INTO PLAYERS (name) VALUES ('Dominik');
